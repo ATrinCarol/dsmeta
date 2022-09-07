97f7ee6e837bcf605b6b9146fd36ae831d6b9c36
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-09-07 04:55:06 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css'
+
+import icon from '../../assets/img/logoDSMeta.svg'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={icon} alt="Logo do app" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.linkedin.com/in/atrindadecarol/" target="_blank">@atrindadecarol</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
