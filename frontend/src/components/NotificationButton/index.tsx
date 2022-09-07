5cd8c78e588bab80523155bf5e457e178f7e6010
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-09-07 04:21:07 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+import icon from '../../assets/img/botaoSMS.svg'
+
+function NotificationButton() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="Notificar" />
+            </div>
+        </>
+    )
+}
+
+export default NotificationButton
