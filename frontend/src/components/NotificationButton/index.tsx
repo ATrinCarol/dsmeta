5612b9f8eda2bd4ae502ca85314390241331efb5
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-09-11 20:38:57 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,25 @@
+import axios from 'axios';
 import './styles.css';
 
 import icon from '../../assets/img/botaoSMS.svg';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("Deu bom");
+        })
+
+}
+
+function NotificationButton({saleId} : Props) {
     return (
         <>
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
                 <img src={icon} alt="Notificar" />
             </div>
         </>
